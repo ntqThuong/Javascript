436c6dc49dfa99f7bc8ae24c31bb670173bc7963
thuong <tn58213@gmail.com>
2024-01-09 09:21:58 +0700

    up

---
+++ BaiTap_2-1/js/script.js
@@ -0,0 +1,16 @@
+// const ngaySinh = prompt("Nhập ngày tháng năm sinh (dd/mm/yyyy): ");
+// const [ngay, thang, nam] = ngaySinh.split("/").map(Number);
+
+// const namHienTai = new Date().getFullYear();
+// const tuoi = namHienTai - nam;
+
+// const message = tuoi >= 18 ? "Đi xem phim nhà bà Nữ" : "Ở nhà coi Doreamon";
+// alert(message);
+
+// function kiemTraChanLe(number) {
+//     alert(number + (number % 2 === 0 ? ' là số chẵn' : ' là số lẻ'));
+//   }
+  
+//   var userInput = prompt('Nhập vào một số: ');
+//   kiemTraChanLe(parseInt(userInput, 10));
+  
